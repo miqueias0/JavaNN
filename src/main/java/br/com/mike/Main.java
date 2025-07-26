36046e7390b58585447b20eecbbbbf31bd85e269
miqueias0 <77019712+miqueias0@users.noreply.github.com>
2025-07-26 10:48:12 -0300

    ++

--- src/main/java/br/com/mike/Main.java
+++ src/main/java/br/com/mike/Main.java
@@ -10,6 +10,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Main {
@@ -17,16 +21,122 @@ public class Main {
         long startTime = System.currentTimeMillis();
         List<List<String>> words = read("train (0).txt", "train (1).txt", "train (2).txt", "train (3).txt", "train (4).txt", "train (5).txt");
         Vocabulario vocab = new Vocabulario();
-        vocab.buildVocabulario(words, 5);
+//        vocab.buildVocabulario(words, 5);
+        vocab.buildVocabularioParquet(5, read("C:\\Users\\Mike\\Desktop\\projetos\\PLN/", "parquet"));
         words = read("train (0).txt");
         GeradorSubWord geradorSubWord = new GeradorSubWord(3);
-        FastText fastText = new FastText(vocab.size(), 64, (int) (2 * Math.pow(10, 6)), 5, geradorSubWord, vocab);
-        fastText.fit(words, 20, 0.025);
+        FastText fastText = new FastText(vocab.size(), 300, (int) (2 * Math.pow(10, 6)), 5, geradorSubWord, vocab);
+        fastText.fit(words, 100, 0.025);
         String[] palavrasTeste = {"rei", "homem", "mulher", "trabalho", "brasil", "grande"};
         fastText.testeSimilaridade(palavrasTeste);
         fastText.testeSimilaridade("rei", "rainha", "princesa", "homem", "mulher");
         fastText.testeAnalogia("rei", "homem", "mulher");
         System.out.println("time: " + (System.currentTimeMillis() - startTime) + "ms");
+        similar(fastText);
+        analogia(fastText);
+    }
+
+
+    private static String[] read(String path, String suffix) throws IOException {
+        Path pasta = Paths.get(path);
+        List<String> arquivosParquet = new ArrayList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(pasta, "*." + suffix)) {
+            for (Path arquivo : stream) {
+                arquivosParquet.add(arquivo.toAbsolutePath().toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return arquivosParquet.toArray(new String[0]);
+    }
+
+    private static void similar(FastText fastText) {
+        // --- Testes de Similaridade ---
+
+        System.out.println("\n--- INICIANDO TESTES DE SIMILARIDADE ---");
+
+// 1. Conceitos Científicos (Astronomia)
+// Esperado: Alta similaridade entre 'universo', 'estrela', 'planeta' e 'galáxia'.
+        fastText.testeSimilaridade("universo", "estrela", "planeta", "galáxia", "átomo");
+
+// 2. Relações Geográficas (Países e Continentes)
+// Esperado: Alta similaridade entre países da América do Sul e o continente.
+        fastText.testeSimilaridade("brasil", "argentina", "chile", "américa", "europa");
+
+// 3. Sinônimos e Conceitos Abstratos
+// Esperado: Alta similaridade entre 'guerra' e 'conflito'. Baixa similaridade com 'paz'.
+        fastText.testeSimilaridade("guerra", "batalha", "conflito", "luta", "paz");
+
+// 4. Ações e Resultados
+// Esperado: Alta similaridade entre 'evolução' e 'desenvolvimento'.
+        fastText.testeSimilaridade("evolução", "desenvolvimento", "mudança", "progresso", "estagnação");
+
+// 5. Política e Sociedade
+// Esperado: Alta similaridade entre 'governo', 'estado' e 'política'.
+        fastText.testeSimilaridade("governo", "estado", "política", "lei", "sociedade");
+
+// 6. Economia e Indústria
+// Esperado: Alta similaridade entre 'economia', 'industrial' e 'comércio'.
+        fastText.testeSimilaridade("economia", "industrial", "comércio", "mercado", "agricultura");
+
+// 7. Animais e Características
+// Esperado: 'ave' deve ter mais similaridade com 'voar' e 'pena' do que com 'nadar'.
+        fastText.testeSimilaridade("ave", "voar", "pena", "bico", "nadar");
+
+// 8. Pessoas e Títulos
+// Esperado: Alta similaridade entre títulos de nobreza.
+        fastText.testeSimilaridade("rei", "rainha", "imperador", "príncipe", "presidente");
+
+        System.out.println("--- FIM DOS TESTES DE SIMILARIDADE ---");
+    }
+
+    private static void analogia(FastText fastText) {
+        // --- Testes de Analogia ---
+
+        System.out.println("\n--- INICIANDO TESTES DE ANALOGIA ---");
+
+// 1. Relação Capital-País
+// "Berlim está para Alemanha, assim como Paris está para ?" -> França
+        fastText.testeAnalogia("alemanha", "berlim", "paris");
+
+// "Lisboa está para Portugal, assim como Buenos Aires está para ?" -> Argentina
+        fastText.testeAnalogia("portugal", "lisboa", "argentina");
+
+// 2. Relação Masculino-Feminino
+// "Homem está para Rei, assim como Mulher está para ?" -> Rainha
+        fastText.testeAnalogia("homem", "rei", "mulher");
+
+// "Príncipe está para Imperador, assim como Princesa está para ?" -> Imperatriz
+        fastText.testeAnalogia("príncipe", "imperador", "princesa");
+
+// 3. Relação Singular-Plural
+// "País está para Países, assim como Cidade está para ?" -> Cidades
+        fastText.testeAnalogia("país", "países", "cidade");
+
+// "Exército está para Exércitos, assim como Soldado está para ?" -> Soldados
+        fastText.testeAnalogia("exército", "exércitos", "soldado");
+
+// 4. Relação Verbo no Infinitivo -> Substantivo
+// "Governar está para Governo, assim como Produzir está para ?" -> Produção
+        fastText.testeAnalogia("governar", "governo", "produzir");
+
+// 5. Relação Objeto-Material
+// "Casa está para Tijolo, assim como Livro está para ?" -> Papel
+        fastText.testeAnalogia("casa", "tijolo", "livro");
+
+// 6. Relação Causa-Efeito
+// "Chuva está para Inundação, assim como Fogo está para ?" -> Incêndio
+        fastText.testeAnalogia("chuva", "inundação", "fogo");
+
+// 7. Relações Científicas
+// "Física está para Einstein, assim como Biologia está para ?" -> Darwin
+        fastText.testeAnalogia("física", "einstein", "biologia");
+
+// "Astronomia está para Telescópio, assim como Biologia está para ?" -> Microscópio
+        fastText.testeAnalogia("astronomia", "telescópio", "biologia");
+
+        System.out.println("--- FIM DOS TESTES DE ANALOGIA ---");
     }
 
     public static String[] embaralhar(List<String> words) {
--- src/main/java/br/com/mike/util/Vocabulario.java
+++ src/main/java/br/com/mike/util/Vocabulario.java
@@ -1,9 +1,17 @@
 package br.com.mike.util;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import br.com.mike.tokenizer.pattern.PatternGPT4;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.avro.AvroReadSupport;
+import org.apache.parquet.hadoop.ParquetReader;
+
+import java.io.IOException;
+import java.util.*;
 
 public class Vocabulario {
 
@@ -12,9 +20,45 @@ public class Vocabulario {
 
     public void buildVocabulario(List<List<String>> sentences, int qtdMinima) {
         Map<String, Integer> frequencia = new HashMap<>();
+        buildVocabulario(sentences, qtdMinima, frequencia);
+    }
+
+    private void buildVocabulario(List<List<String>> sentences, int qtdMinima, Map<String, Integer> frequencia) {
         for (List<String> sentence : sentences) {
             for (String word : sentence) {
                 frequencia.merge(word, 1, Integer::sum);
+            }
+        }
+    }
+
+    public void buildVocabularioParquet(int qtdMinima, String... paths) {
+        Map<String, Integer> frequencia = new HashMap<>();
+        int length = 500000;
+        PatternGPT4 patternGPT4 = new PatternGPT4();
+        for (String path : paths) {
+            Configuration conf = new Configuration();
+            conf.setBoolean("parquet.avro.schema.leniency", true);
+            Schema projectionSchema = SchemaBuilder.record("MeuSchema")
+                    .namespace("br.com.mike")
+                    .fields()
+                    .name("content").type().nullable().stringType().noDefault()
+                    .endRecord();
+            AvroReadSupport.setRequestedProjection(conf, projectionSchema);
+            try (ParquetReader<GenericRecord> reader = AvroParquetReader.<GenericRecord>builder(new Path(path))
+                    .withConf(conf)
+                    .build()) {
+                GenericRecord record;
+                int i = 0;
+                while ((record = reader.read()) != null) {
+                    buildVocabulario(Arrays.asList(record.get("content").toString().split("\n")).parallelStream().map(word -> Arrays.asList(patternGPT4.split(word))).toList(), qtdMinima, frequencia);
+                    i++;
+                    if (i % 1000 == 0) {
+                        System.out.println("Processed: " + i + "/" + length + " percent: " + ((double) i / length * 100) + "%");
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Erro ao ler o arquivo Parquet: " + e.getMessage());
+                e.printStackTrace();
             }
         }
         for (Map.Entry<String, Integer> entry : frequencia.entrySet()) {
